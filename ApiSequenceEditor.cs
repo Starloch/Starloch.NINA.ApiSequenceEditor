fc2b4d6757cb4ab2e1d250dd62ee9726690b83b9
YoruDev-Ryland <159859604+YoruDev-Ryland@users.noreply.github.com>
2025-02-10 20:31:43 -0600

    Working WebUI

--- ApiSequenceEditor.cs
+++ ApiSequenceEditor.cs
@@ -1,140 +1,270 @@
-﻿using NINA.Core.Model;
-using NINA.Core.Utility;
-using NINA.Image.ImageData;
-using NINA.Plugin;
-using NINA.Plugin.Interfaces;
-using NINA.Profile;
-using NINA.Profile.Interfaces;
-using NINA.WPF.Base.Interfaces.Mediator;
-using NINA.WPF.Base.Interfaces.ViewModel;
-using Starloch.NINA.ApiSequenceEditor.Properties;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
-using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
-using Settings = Starloch.NINA.ApiSequenceEditor.Properties.Settings;
+using System.Windows.Input;
+using NINA.Plugin;
+using NINA.Plugin.Interfaces;
+using NINA.Sequencer.Mediator;
+using NINA.Profile;
+using NINA.Profile.Interfaces;
+using NINA.Sequencer.Interfaces.Mediator;
 
-namespace Starloch.NINA.ApiSequenceEditor {
-    /// <summary>
-    /// This class exports the IPluginManifest interface and will be used for the general plugin information and options
-    /// The base class "PluginBase" will populate all the necessary Manifest Meta Data out of the AssemblyInfo attributes. Please fill these accoringly
-    /// 
-    /// An instance of this class will be created and set as datacontext on the plugin options tab in N.I.N.A. to be able to configure global plugin settings
-    /// The user interface for the settings will be defined by a DataTemplate with the key having the naming convention "ApiSequenceEditor_Options" where ApiSequenceEditor corresponds to the AssemblyTitle - In this template example it is found in the Options.xaml
-    /// </summary>
+namespace Starloch.NINA.ApiSequenceEditor
+{
     [Export(typeof(IPluginManifest))]
-    public class ApiSequenceEditor : PluginBase, INotifyPropertyChanged {
-        private readonly IPluginOptionsAccessor pluginSettings;
-        private readonly IProfileService profileService;
-        private readonly IImageSaveMediator imageSaveMediator;
+    public class ApiSequenceEditor : PluginBase, INotifyPropertyChanged
+    {
+        private readonly ISequenceMediator _sequenceMediator;
+        private readonly IPluginOptionsAccessor _pluginSettings;
 
-        // Implementing a file pattern
-        private readonly ImagePattern exampleImagePattern = new ImagePattern("$$EXAMPLEPATTERN$$", "An example of an image pattern implementation", "API Sequence Editor");
+        private HttpListener _listener;
+        private CancellationTokenSource _cts;
+        private Task _listenerTask;
+
+        private bool _webServerEnabled;
+        private int _port;
+        private string _serverUrls;
 
         [ImportingConstructor]
-        public ApiSequenceEditor(IProfileService profileService, IOptionsVM options, IImageSaveMediator imageSaveMediator) {
-            if (Settings.Default.UpdateSettings) {
-                Settings.Default.Upgrade();
-                Settings.Default.UpdateSettings = false;
-                CoreUtil.SaveSettings(Settings.Default);
-            }
+        public ApiSequenceEditor(ISequenceMediator sequenceMediator, IProfileService profileService)
+        {
+            _sequenceMediator = sequenceMediator;
+            _pluginSettings = new PluginOptionsAccessor(profileService, Guid.Parse(this.Identifier));
 
-            // This helper class can be used to store plugin settings that are dependent on the current profile
-            this.pluginSettings = new PluginOptionsAccessor(profileService, Guid.Parse(this.Identifier));
-            this.profileService = profileService;
-            // React on a changed profile
-            profileService.ProfileChanged += ProfileService_ProfileChanged;
+            // Load settings from plugin options
+            LoadSettings();
 
-            // Hook into image saving for adding FITS keywords or image file patterns
-            this.imageSaveMediator = imageSaveMediator;
+            RestartServerCommand = new RelayCommand(RestartHttpServer);
 
-            // Run these handlers when an image is being saved
-            this.imageSaveMediator.BeforeImageSaved += ImageSaveMediator_BeforeImageSaved;
-            this.imageSaveMediator.BeforeFinalizeImageSaved += ImageSaveMediator_BeforeFinalizeImageSaved;
-
-            // Register a new image file pattern for the Options > Imaging > File Patterns area
-            options.AddImagePattern(exampleImagePattern);
-        }
-
-        public override Task Teardown() {
-            // Make sure to unregister an event when the object is no longer in use. Otherwise garbage collection will be prevented.
-            profileService.ProfileChanged -= ProfileService_ProfileChanged;
-            imageSaveMediator.BeforeImageSaved -= ImageSaveMediator_BeforeImageSaved;
-            imageSaveMediator.BeforeFinalizeImageSaved -= ImageSaveMediator_BeforeFinalizeImageSaved;
-
-            return base.Teardown();
-        }
-
-        private void ProfileService_ProfileChanged(object sender, EventArgs e) {
-            // Rase the event that this profile specific value has been changed due to the profile switch
-            RaisePropertyChanged(nameof(ProfileSpecificNotificationMessage));
-        }
-
-        private Task ImageSaveMediator_BeforeImageSaved(object sender, BeforeImageSavedEventArgs e) {
-            // Insert the example FITS keyword of a specific data type into the image metadata object prior to the file being saved
-            // FITS keywords have a maximum of 8 characters. Comments are options. Comments that are too long will be truncated.
-
-            string exampleKeywordComment = "This is a {0} keyword";
-
-            // string
-            string exampleStringKeywordName = "STRKEYWD";
-            string exampleStringKeywordValue = "Example";
-            e.Image.MetaData.GenericHeaders.Add(new StringMetaDataHeader(exampleStringKeywordName, exampleStringKeywordValue, string.Format(exampleKeywordComment, "string")));
-
-            // integer
-            string exampleIntKeywordName = "INTKEYWD";
-            int exampleIntKeywordValue = 5;
-            e.Image.MetaData.GenericHeaders.Add(new IntMetaDataHeader(exampleIntKeywordName, exampleIntKeywordValue, string.Format(exampleKeywordComment, "integer")));
-
-            // double
-            string exampleDoubleKeywordName = "DBLKEYWD";
-            double exampleDoubleKeywordValue = 1.3d;
-            e.Image.MetaData.GenericHeaders.Add(new DoubleMetaDataHeader(exampleDoubleKeywordName, exampleDoubleKeywordValue, string.Format(exampleKeywordComment, "double")));
-
-            // Classes also exist for other data types:
-            // BoolMetaDataHeader()
-            // DateTimeMetaDataHeader()
-
-            return Task.CompletedTask;
-        }
-
-        private Task ImageSaveMediator_BeforeFinalizeImageSaved(object sender, BeforeFinalizeImageSavedEventArgs e) {
-            // Populate the example image pattern with data. This can provide data that may not be immediately available
-            e.AddImagePattern(new ImagePattern(exampleImagePattern.Key, exampleImagePattern.Description, exampleImagePattern.Category) {
-                Value = $"{DateTime.Now:yyyy-MM-ddTHH:mm:ss.ffffffK}"
-            });
-
-            return Task.CompletedTask;
-        }
-
-        public string DefaultNotificationMessage {
-            get {
-                return Settings.Default.DefaultNotificationMessage;
-            }
-            set {
-                Settings.Default.DefaultNotificationMessage = value;
-                CoreUtil.SaveSettings(Settings.Default);
-                RaisePropertyChanged();
+            if (WebServerEnabled)
+            {
+                StartHttpServer();
             }
         }
 
-        public string ProfileSpecificNotificationMessage {
-            get {
-                return pluginSettings.GetValueString(nameof(ProfileSpecificNotificationMessage), string.Empty);
+        #region Plugin Settings
+
+        private void LoadSettings()
+        {
+            // For older versions of the plugin system, only GetValueString/SetValueString may exist
+            var enabledStr = _pluginSettings.GetValueString(nameof(WebServerEnabled), "false");
+            _webServerEnabled = bool.TryParse(enabledStr, out bool en) && en;
+
+            var portStr = _pluginSettings.GetValueString(nameof(Port), "1999");
+            if (!int.TryParse(portStr, out _port))
+            {
+                _port = 1999;
             }
-            set {
-                pluginSettings.SetValueString(nameof(ProfileSpecificNotificationMessage), value);
-                RaisePropertyChanged();
+
+            _serverUrls = $"http://localhost:{_port}/debug";
+        }
+
+        private void SaveSettings()
+        {
+            // Store the bool and int as strings
+            _pluginSettings.SetValueString(nameof(WebServerEnabled), _webServerEnabled.ToString());
+            _pluginSettings.SetValueString(nameof(Port), _port.ToString());
+            RaisePropertyChanged(nameof(WebServerEnabled));
+            RaisePropertyChanged(nameof(Port));
+            RaisePropertyChanged(nameof(ServerUrls));
+        }
+
+        /// <summary>
+        /// True/False controlling whether the HTTP server should run.
+        /// </summary>
+        public bool WebServerEnabled
+        {
+            get => _webServerEnabled;
+            set
+            {
+                if (_webServerEnabled != value)
+                {
+                    _webServerEnabled = value;
+                    if (_webServerEnabled)
+                    {
+                        StartHttpServer();
+                    }
+                    else
+                    {
+                        StopHttpServer();
+                    }
+                    SaveSettings();
+                    RaisePropertyChanged();
+                }
             }
         }
+
+        /// <summary>
+        /// Port for the HTTP server.
+        /// </summary>
+        public int Port
+        {
+            get => _port;
+            set
+            {
+                if (_port != value)
+                {
+                    _port = value;
+                    RestartHttpServer();
+                    SaveSettings();
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shows either "Server Stopped" or the URL if running.
+        /// </summary>
+        public string ServerUrls
+        {
+            get => _serverUrls;
+            private set
+            {
+                if (_serverUrls != value)
+                {
+                    _serverUrls = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region HTTP Server Logic
+
+        private void StartHttpServer()
+        {
+            try
+            {
+                StopHttpServer(); // Make sure any existing listener is closed
+
+                _listener = new HttpListener();
+                // Bind only to localhost for fewer permission issues:
+                _listener.Prefixes.Add($"http://localhost:{_port}/");
+                _listener.Prefixes.Add($"http://127.0.0.1:{_port}/");
+
+                _listener.Start();
+                _cts = new CancellationTokenSource();
+                _listenerTask = Task.Run(() => HandleRequests(_cts.Token));
+
+                ServerUrls = $"http://localhost:{_port}/debug";
+            }
+            catch (HttpListenerException ex)
+            {
+                ServerUrls = $"Failed to start: {ex.Message}";
+            }
+        }
+
+        private void StopHttpServer()
+        {
+            _cts?.Cancel();
+            if (_listener != null && _listener.IsListening)
+            {
+                _listener.Stop();
+                _listener.Close();
+                _listener = null;
+            }
+            ServerUrls = "Server Stopped";
+        }
+
+        private async Task HandleRequests(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    var context = await _listener.GetContextAsync();
+                    _ = Task.Run(() => ProcessRequest(context), token);
+                }
+                catch
+                {
+                    if (token.IsCancellationRequested) break;
+                }
+            }
+        }
+
+        private async Task ProcessRequest(HttpListenerContext context)
+        {
+            try
+            {
+                string responseString = "";
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                if (context.Request.HttpMethod == "GET" && context.Request.Url.AbsolutePath == "/debug")
+                {
+                    var debugInfo = _sequenceMediator.GetAllTargetsInAdvancedSequence();
+                    responseString = JsonSerializer.Serialize(debugInfo, new JsonSerializerOptions { WriteIndented = true });
+                    response.StatusCode = 200;
+                }
+                else
+                {
+                    response.StatusCode = 404;
+                    responseString = JsonSerializer.Serialize(new { error = "Endpoint not found" });
+                }
+
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+                response.OutputStream.Close();
+            }
+            catch
+            {
+                context.Response.StatusCode = 500;
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand RestartServerCommand { get; }
+
+        public void RestartHttpServer()
+        {
+            StopHttpServer();
+            if (WebServerEnabled)
+            {
+                StartHttpServer();
+            }
+            SaveSettings();
+        }
+
+        #endregion
+
+        #region PluginBase
+
+        public override async Task Teardown()
+        {
+            StopHttpServer();
+            await base.Teardown();
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #endregion
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        public RelayCommand(Action execute) => _execute = execute;
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => _execute();
     }
 }
